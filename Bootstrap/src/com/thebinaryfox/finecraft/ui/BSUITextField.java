7552464684b06a6dea5bea662df370f071de9fc2
0b F0x <adb03f61@opayq.com>
2013-07-04 12:14:18 -0700

    Finished the graphical part of the welcome UI

--- Bootstrap/src/com/thebinaryfox/finecraft/ui/BSUITextField.java
+++ Bootstrap/src/com/thebinaryfox/finecraft/ui/BSUITextField.java
@@ -1,5 +1,48 @@
 package com.thebinaryfox.finecraft.ui;
 
-public class BSUITextField {
-	
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JTextField;
+
+public class BSUITextField extends JTextField {
+
+	private static final long serialVersionUID = -1100884327443439632L;
+
+	// Fields
+	private String placeholder;
+
+	// Constructors
+	public BSUITextField(String placeholder) {
+		super("");
+		
+		this.placeholder = placeholder;
+	}
+
+	// Public methods
+	public void setPlaceholder(String placeholder) {
+		this.placeholder = placeholder;
+	}
+
+	public String getPlaceholder() {
+		return placeholder;
+	}
+
+	// Overridden methods
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		if (getText().isEmpty()) {
+			if (g instanceof Graphics2D) {
+				((Graphics2D) g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+			}
+
+			g.setColor(getDisabledTextColor());
+			g.setFont(getFont());
+			g.drawString(placeholder, getInsets().left, getHeight() / 2 + getInsets().top - 1);
+		}
+	}
+
 }
--- Bootstrap/src/com/thebinaryfox/finecraft/ui/UIBootstrapWelcome.java
+++ Bootstrap/src/com/thebinaryfox/finecraft/ui/UIBootstrapWelcome.java
@@ -2,6 +2,10 @@ package com.thebinaryfox.finecraft.ui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
@@ -13,7 +17,11 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SpringLayout;
+import javax.swing.border.Border;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  * The welcome window where you select a launcher.
@@ -22,15 +30,20 @@ import javax.swing.SpringLayout;
  */
 public class UIBootstrapWelcome extends BSUIWindow {
 
-	static final Color background = new Color(235, 235, 235);
-	static final Color selection = new Color(220, 220, 220);
-	static final Color text = new Color(100, 100, 100);
+	static private final Color background = new Color(235, 235, 235);
+	static private final Color selection = new Color(220, 220, 220);
+	static private final Color text = new Color(100, 100, 100);
+	static private final Border other_fail = BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200, 100, 100)), BorderFactory.createEmptyBorder(5, 5, 5, 5));
+	static private final Border other_okay = BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(text), BorderFactory.createEmptyBorder(5, 5, 5, 5));
+	static private final Color other_failc = new Color(255, 210, 210);
+	static private final Color other_okayc = Color.white;
 
 	private JLabel title;
 	private JLabel titletext;
 	private JList recommended;
 	private JTextField other;
 	private JButton use;
+	private JScrollPane recommendedp;
 	private HashMap<String, String> recommendedurls;
 	private DefaultListModel mod;
 
@@ -46,17 +59,79 @@ public class UIBootstrapWelcome extends BSUIWindow {
 		titletext.setFont(titletext.getFont().deriveFont(13f));
 		titletext.setForeground(text);
 
+		other = new BSUITextField("URL");
+		other.setBorder(other_okay);
+		other.setBackground(other_okayc);
+		other.setForeground(Color.black);
+		other.setDisabledTextColor(Color.gray);
+		other.setVisible(false);
+		other.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (other.getText().trim().isEmpty())
+					return;
+
+				try {
+					other.setDisabledTextColor(Color.gray);
+					other.setBackground(other_okayc);
+					other.setBorder(other_okay);
+					check(new URL(other.getText()));
+				} catch (Exception ex) {
+					other.setDisabledTextColor(new Color(200, 100, 100));
+					other.setBackground(other_failc);
+					other.setBorder(other_fail);
+				}
+			}
+
+		});
+
+		use = new JButton("Select Launcher");
+		use.setOpaque(true);
+		use.setForeground(Color.black);
+		use.setBackground(new Color(215, 215, 215));
+		use.setBorder(other_okay);
+		use.setVisible(false);
+
 		mod = new DefaultListModel();
-		mod.addElement("`dLoading...");
+		mod.addElement("`dDownloading...");
 		mod.addElement("Other");
 
 		recommended = new BSUIList();
 		recommended.setModel(mod);
 		recommended.setSelectionBackground(selection);
 		recommended.setSelectionForeground(text);
+		recommended.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		recommended.setBorder(BorderFactory.createEmptyBorder());
 		recommended.setBackground(Color.white);
-		JScrollPane recommendedp = new JScrollPane(recommended);
+		recommended.addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent arg0) {
+				if (recommended.getSelectedIndex() == -1) {
+					other.setVisible(false);
+					use.setVisible(false);
+					getLayout().putConstraint("South", recommendedp, -20, "South", getPanel());
+					getPanel().validate();
+					return;
+				}
+
+				if (recommended.getSelectedValue().toString().equals("Other")) {
+					other.setVisible(true);
+					use.setVisible(false);
+					getLayout().putConstraint("South", recommendedp, -5, "North", other);
+				} else {
+					other.setVisible(false);
+					use.setVisible(true);
+					getLayout().putConstraint("South", recommendedp, -5, "North", use);
+				}
+
+				getPanel().validate();
+			}
+
+		});
+
+		recommendedp = new JScrollPane(recommended);
 		recommendedp.setBorder(BorderFactory.createLineBorder(text));
 
 		recommended_download();
@@ -67,6 +142,8 @@ public class UIBootstrapWelcome extends BSUIWindow {
 		container.add(title);
 		container.add(titletext);
 		container.add(recommendedp);
+		container.add(other);
+		container.add(use);
 
 		// Layout
 		SpringLayout layout = getLayout();
@@ -80,11 +157,24 @@ public class UIBootstrapWelcome extends BSUIWindow {
 		layout.putConstraint("North", recommendedp, 10, "South", titletext);
 		layout.putConstraint("West", recommendedp, 20, "West", container);
 		layout.putConstraint("East", recommendedp, -20, "East", container);
+		layout.putConstraint("South", recommendedp, -20, "South", container);
+
+		layout.putConstraint("West", other, 20, "West", container);
+		layout.putConstraint("East", other, -20, "East", container);
+		layout.putConstraint("South", other, -20, "South", container);
+
+		layout.putConstraint("West", use, 20, "West", container);
+		layout.putConstraint("East", use, -20, "East", container);
+		layout.putConstraint("South", use, -20, "South", container);
 	}
 
 	@Override
 	protected boolean animate() {
 		return true;
+	}
+
+	private void check(URL url) throws IOException {
+		// TODO check if valid launcher update cast
 	}
 
 	private void recommended_download() {
@@ -98,7 +188,7 @@ public class UIBootstrapWelcome extends BSUIWindow {
 
 				recommendedurls = new LinkedHashMap<String, String>();
 				for (int i = 0; i < 10; i++) {
-					recommendedurls.put("Debug Item #", "-");
+					recommendedurls.put("Debug Item #" + i, "-");
 				}
 
 				// Animate
